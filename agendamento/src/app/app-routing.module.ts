76f5d4269aa4bbe63d1463eafbd7779fe38f3bbe
João Pedro <joaokopernico>
2022-09-21 14:03:20 -0300

    Frontend Criado

---
+++ agendamento/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { FormAgendarComponent } from './components/form-agendar/form-agendar.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: "agendar", component: FormAgendarComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ agendamento/src/app/components/form-agendar/form-agendar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-form-agendar',
+  templateUrl: './form-agendar.component.html',
+  styleUrls: ['./form-agendar.component.css']
+})
+export class FormAgendarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
